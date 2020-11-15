e8dd399b685adb75392d395ba30f32f4c0f8aed0
Amitav Roy <reachme@amitavroy.com>
2020-11-15 11:55:47 +0530

    Adding Laravel UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Amitav Roy',
+            'email' => 'reachme@amitavroy.com',
+            'password' => \bcrypt('password'),
+        ]);
     }
 }
