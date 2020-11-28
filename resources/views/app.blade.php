1b45d559a90b86fb5ca509a93d5aa6fb15a67b4f
Amitav Roy <reachme@amitavroy.com>
2020-11-28 10:31:55 +0530

    Changing the documentation and other basic stuff

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
     @routes
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+
   </head>
   <body>
     @inertia
